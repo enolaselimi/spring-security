94cc7e5582125fb8bf9629fb86b170c52c5096d1
Joan Janku <janku.joan4@gmail.com>
2023-10-25 01:29:13 +0200

    final

--- src/main/java/com/joan/security/config/Paths.java
+++ src/main/java/com/joan/security/config/Paths.java
@@ -3,7 +3,7 @@ package com.joan.security.config;
 public enum Paths {
     LOGIN("/v1/auth/log-in"), SIGN_UP("/v1/auth/sign-up");
 
-    private String value;
+    private final String value;
 
     Paths(String value){
         this.value = value;
