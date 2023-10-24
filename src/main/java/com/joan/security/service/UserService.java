f73c79b30b19e45b16e0fcfc398a7d2f13fd8e4a
Joan Janku <janku.joan4@gmail.com>
2023-10-25 00:24:00 +0200

    refactor

--- src/main/java/com/joan/security/service/UserService.java
+++ src/main/java/com/joan/security/service/UserService.java
@@ -7,16 +7,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Collections;
 
 @Service
 public class UserService implements UserDetailsService {
     private AuthenticationService authenticationService;
 
-    public UserDTO signUp(SignUpDTO signUpDTO){
-        return new UserDTO(1l,signUpDTO.getFirstName(),signUpDTO.getLastName(),signUpDTO.getLogin(),"token", Collections.EMPTY_LIST);
+    public UserDTO signUp(SignUpDTO signUpDTO) {
+        return new UserDTO(1L, signUpDTO.getFirstName(), signUpDTO.getLastName(), signUpDTO.getLogin(), "token", Collections.EMPTY_LIST);
     }
 
     @Override
