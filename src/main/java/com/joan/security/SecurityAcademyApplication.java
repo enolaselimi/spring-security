706c07040f6dc1a84b5e96d04f1bd269e9b69eb6
Joan Janku <janku.joan4@gmail.com>
2023-10-24 18:25:24 +0200

    disable default config

--- src/main/java/com/joan/security/SecurityAcademyApplication.java
+++ src/main/java/com/joan/security/SecurityAcademyApplication.java
@@ -2,8 +2,10 @@ package com.joan.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class SecurityAcademyApplication {
 
     public static void main(String[] args) {
